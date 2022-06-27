e3f8a689f9ffd09ceda7c9c87c596dc3308d4ddc
Muhammad Al Juburi <maljuburi89@gmail.com>
2022-06-27 14:33:17 -0700

    feat(http): add health check for http

---
+++ http-health.js
@@ -0,0 +1,13 @@
+const http = require('http')
+module.exports = {
+  httpHealth: () => {
+    const requestListener = function (req, res) {
+      res.writeHead(200)
+      res.end('healthy')
+    }
+
+    const server = http.createServer(requestListener)
+    server.listen(5000)
+    console.log('http listening on port 5000')
+  },
+}
